41b5b982c26f9a749d5bf2f28056ea6958c9ce83
armaabetancourtt <armabetancourtt@gmail.com>
2025-08-15 09:45:06 -0600

    Nodo

--- listas/Node.java
+++ listas/Node.java
@@ -16,12 +16,13 @@ package listas;
  * @param next Referencia al siguiente nodo de la lista (puede ser null).
  */
 
-//Definición de clase Node: Es un nodo en una lista entrelazada
+//Definición de clase Node: Es un nodo para los diferentes tipos de listas entrelazadas
 
 public class Node <E> {
     //Atributos
     private E data;
     Node next;
+    Node prev; //Para listas doblemente entrelazadas
     
     /** 
      * Constructor que crea un nodo con un dato y una para al siguiente nodo
@@ -31,6 +32,7 @@ public class Node <E> {
     public Node (E data, Node next) {
         this.data=data;
         this.next=next; //Puede dar nulo
+        this.prev=null;
     }
     
     /**
@@ -40,6 +42,7 @@ public class Node <E> {
     public Node (E data) {
         this.data=data;
         this.next=null;
+        this.prev=null;
     }
     
     /**
@@ -62,8 +65,16 @@ public class Node <E> {
      * Obtiene el nodo siguiente en la lista
      * @return Nodo siguiente
     */
-    public Node<E> next() {
+    public Node<E> getNext() {
         return next; //En el último será nulo
+    }
+    
+        /**
+     * Obtiene el nodo previo en la lista
+     * @return Nodo previo
+    */
+    public Node<E> getPrev() {
+        return prev;
     }
     
     /**
@@ -73,6 +84,15 @@ public class Node <E> {
     public void setNext (Node<E> next) {
         this.next=next;
     }
+    
+    /**
+     * Setter del nodo previo  en la lista
+     * @param prev (el nodo previo)
+    */
+    public void setPrev (Node<E> prev) {
+        this.prev=prev;
+    }
 }
+
 
 
