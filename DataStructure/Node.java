a8d836cb3115ac8ddc01b00c6eda189110e186fc
armaabetancourtt <armabetancourtt@gmail.com>
2025-08-15 13:54:08 -0600

    Node

--- DataStructure/Node.java
+++ DataStructure/Node.java
@@ -8,7 +8,7 @@
 * devolver o cambiar el número o dato que esta guardando el nodo.
 **/
 
-package DataStructures;
+package DataStructure;
 
 /**
  * Constructor que crea un nodo con un dato y una referencia al siguiente nodo.
