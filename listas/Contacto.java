2b16b2dc4f02d8e7e032ecbc57dcfec124ed8d41
armaabetancourtt <armabetancourtt@gmail.com>
2025-08-15 10:19:40 -0600

    Contacto

--- listas/Contacto.java
+++ listas/Contacto.java
@@ -28,6 +28,16 @@ public class Contacto {
             this.telefono = telefono;
         }
 
+        // Getters y Setters
+        public String getNombre() { return nombre; }
+        public void setNombre(String nombre) { this.nombre = nombre; }
+
+        public String getDireccion() { return direccion; }
+        public void setDireccion(String direccion) { this.direccion = direccion; }
+
+        public String getTelefono() { return telefono; }
+        public void setTelefono(String telefono) { this.telefono = telefono; }
+
         @Override
         public String toString() {
             return nombre + " , " + direccion + " , " + telefono;
