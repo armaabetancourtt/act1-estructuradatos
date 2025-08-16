c156c2f399ca842f982754b0078c497402b9dc67
armaabetancourtt <armabetancourtt@gmail.com>
2025-08-15 18:33:48 -0600

    Node

--- DataStructure/Node.java
+++ DataStructure/Node.java
@@ -1,100 +1,80 @@
 /**
-* Autor: Armando Betancourt Esparza
-* Fecha: 14/08/2025
-* Descripción: Esta clase representa un nodo de listas entrelazadas, a mi propio razionamiento es una manera 
-* más eficiente que estoy usando para guardar cualquier tipo de dato (Gracias a <E>), formando una lista de nodos,
-* donde si next es nulo el programa interpreta que es el último de la lista de nodos, uso constructores para crear
-* un nodo con dato y así asignarlo a otro (como una cadena de nodos o "vagones" de tren) e integro métodos para
-* devolver o cambiar el número o dato que esta guardando el nodo.
-**/
-
+ * Autor: Armando Betancourt Esparza
+ * Fecha: 14/08/2025
+ * Descripción: Nodo para listas enlazadas, su función es ser la base para la construcción de las listas entrelazadas
+ * 
+ */
 package DataStructure;
 
-/**
- * Constructor que crea un nodo con un dato y una referencia al siguiente nodo.
- * @param data Valor que se almacenará en el nodo.
- * @param next Referencia al siguiente nodo de la lista (puede ser null).
- */
-
-//Definición de clase Node: Es un nodo para los diferentes tipos de listas entrelazadas
-
-public class Node <E> {
-    //Atributos
+public class Node<E> {
+    /** Dato almacenado en el nodo y referencias al siguiente y previo nodo */
     private E data;
-    Node next;
-    Node prev; //Para listas doblemente entrelazadas
-    
-    /** 
-     * Constructor que crea un nodo con un dato y una para al siguiente nodo
-     * @param data (dato) que se guardará en el nodo
-     * @param next Es como una referencia a lo que es al siguiente nodo de la lista
-    */
-    public Node (E data, Node next) {
-        this.data=data;
-        this.next=next; //Puede dar nulo
-        this.prev=null;
-    }
-    
+    Node<E> next;
+    Node<E> prev;
+
     /**
-     * Constructor que crea un nodo con un dato y sin nodo siguiente
-     * @param data (dato) que se almacenará en el nodo
-    */
-    public Node (E data) {
-        this.data=data;
-        this.next=null;
-        this.prev=null;
+     * Constructor que inicializa un nodo con un dato
+     * Las referencias al siguiente y al anterior nodo inician en en null
+     *
+     * @param data El dato que guardará el nodo.
+     */
+    public Node(E data) {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
     }
-    
+
     /**
-     * Getter del dato que se guardo en el nodo
-     * @return Dato de tipo E
-    */
+     * Obtiene el dato guardado en el nodo
+     *
+     * @return El dato del nodo
+     */
     public E getData() {
         return data;
     }
-    
+
     /**
-     * Modifica y reemplaza el dato almacenado en el nodo
-     * @param data será el nuevo valor que reemplazará al anterior
-    */
-    public void setData (E data){
-        this.data=data;
+     * Establece o actualiza el dato almacenado en el nodo
+     *
+     * @param data - El nuevo dato a almacenar
+     */
+    public void setData(E data) {
+        this.data = data;
     }
-    
+
     /**
-     * Obtiene el nodo siguiente en la lista
-     * @return Nodo siguiente
-    */
+     * Obtiene la referencia al siguiente nodo
+     *
+     * @return El nodo siguiente, o null si no existe.
+     */
     public Node<E> getNext() {
-        return next; //En el último será nulo
+        return next;
     }
-    
-        /**
-     * Obtiene el nodo previo en la lista
-     * @return Nodo previo
-    */
+
+    /**
+     * Establece la referencia al siguiente nodo
+     *
+     * @param next El nodo siguiente
+     */
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    /**
+     * Obtiene la referencia al nodo anterior (solo aplica para listas dobles)
+     *
+     * @return El nodo anterior, o da como resultado null si no exist
+     */
     public Node<E> getPrev() {
         return prev;
     }
-    
+
     /**
-     * Setter del siguiente nodo  en la lista
-     * @param next (el siguiente nodo)
-    */
-    public void setNext (Node<E> next) {
-        this.next=next;
-    }
-    
-    /**
-     * Setter del nodo previo  en la lista
-     * @param prev (el nodo previo)
-    */
-    public void setPrev (Node<E> prev) {
-        this.prev=prev;
+     * Establece (Set) la referencia al nodo anterior (solo para listas dobles)
+     *
+     * @param prev El nodo anterior
+     */
+    public void setPrev(Node<E> prev) {
+        this.prev = prev;
     }
 }
-
-
-
-
-
